5cda2400fc139567abaf77adb68386f32884c659
sanjeev <sanjeev@sursatech.com>
2024-01-01 23:23:35 +0545

    init

---
+++ src/main/java/jp/co/fsz/clounect/ClounectApplication.java
@@ -0,0 +1,13 @@
+package jp.co.fsz.clounect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClounectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClounectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/co/fsz/clounect/ClounectApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.fsz.clounect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClounectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
